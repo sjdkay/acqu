1f309f6cbba426953c2675a8be410dfcf10b8354
Andreas Neiser <neiser@kph.uni-mainz.de>
2013-11-14 08:18:51 +0100

    User: PID updates, linPolMacro, TAPS Offline

--- acqu_user/root/macros.online/linPolMacro.C
+++ acqu_user/root/macros.online/linPolMacro.C
@@ -4,7 +4,8 @@ void linPolMacro(){
   FILE  *fp=fopen("/exports/a2raid5/tagscaler.txt","w");
   for(int n=1;n<=352;n++){
     //    fprintf(fp,"%d\n", FPD_ScalerCurr->GetBinContent(353-n));
-    fprintf(fp,"%d\n", FPD_ScalerAcc->GetBinContent(n));
+    fprintf(fp,"%d\n", FPD_ScalerCurr->GetBinContent(n));
+    //fprintf(fp,"%d\n", FPD_ScalerAcc->GetBinContent(n));
   }
   fclose(fp);
 }
