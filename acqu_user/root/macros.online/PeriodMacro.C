e681b3c10c5f41ded2c5e86cf4dffd885584aca1
Andreas Neiser <neiser@kph.uni-mainz.de>
2014-03-28 14:00:15 +0100

    User: Renaming particular linPolMacro to PeriodMacro, will serve more purposes

--- acqu_user/root/macros.online/PeriodMacro.C
+++ acqu_user/root/macros.online/PeriodMacro.C
@@ -1,12 +1,12 @@
-//Dump the current FP scalers to a nfs exported file system
+// This code gets executed by the online analysis
+// every Nth events (see online config Period: statement)
 
-void linPolMacro(){
+void PeriodMacro(){
+  // Dump the current FP scalers to a nfs exported file system
   if((FPD_ScalerCurr->Integral()) > 0){
     FILE  *fp=fopen("/exports/a2raid5/tagscaler.txt","w");
     for(int n=1;n<=352;n++){
-      //    fprintf(fp,"%d\n", FPD_ScalerCurr->GetBinContent(353-n));
       fprintf(fp,"%d\n", FPD_ScalerCurr->GetBinContent(n));
-      //fprintf(fp,"%d\n", FPD_ScalerAcc->GetBinContent(n));
     }
     fclose(fp);
   }
