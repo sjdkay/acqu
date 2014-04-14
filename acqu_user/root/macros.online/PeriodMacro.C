7d30478cf62296dc5477c5a165e88353d1be102a
Phil Martel <philmartel0@gmail.com>
2014-04-14 11:18:35 +0200

    User: Add EventsWithError to EPICS online monitoring

--- acqu_user/root/macros.online/PeriodMacro.C
+++ acqu_user/root/macros.online/PeriodMacro.C
@@ -3,28 +3,44 @@
 
 void PeriodMacro() {
   // Dump the current FP scalers to a nfs exported file system
-  if((FPD_ScalerCurr->Integral()) > 0){
-    FILE  *fp=fopen("/exports/a2raid5/tagscaler.txt","w");
-    for(int n=1;n<=352;n++){
-      fprintf(fp,"%d\n", FPD_ScalerCurr->GetBinContent(n));
+  if(gROOT->FindObject("FPD_ScalerCurr")){
+    if((FPD_ScalerCurr->Integral()) > 0){
+      FILE  *fp=fopen("scratch/tagscaler.txt","w");
+      //FILE  *fp=fopen("/exports/a2raid5/tagscaler.txt","w");
+      for(int n=1;n<=352;n++){
+	fprintf(fp,"%d\n", FPD_ScalerCurr->GetBinContent(n));
+      }
+      fclose(fp);
     }
-    fclose(fp);
   }
-  
+
   // calculate the background-subtracted pairspec data
-  if((PairSpec_Open->Integral())>0) {
-    stringstream cmd;
-    cmd << "caput -a BEAM:PAIRSPEC:TaggEff 352";
-    for(int n=1; n<=352; n++) {
-      Double_t open = PairSpec_Open->GetBinContent(n);
-      Double_t gated = PairSpec_Gated->GetBinContent(n);
-      Double_t gated_dly = PairSpec_GatedDly->GetBinContent(n);
-      Double_t taggeff = (gated-gated_dly)/open;
-      if(!TMath::Finite(taggeff))
+  if(gROOT->FindObject("PairSpec_Open")){
+    if((PairSpec_Open->Integral())>0) {
+      stringstream cmd;
+      cmd << "caput -a BEAM:PAIRSPEC:TaggEff 352";
+      for(int n=1; n<=352; n++) {
+	Double_t open = PairSpec_Open->GetBinContent(n);
+	Double_t gated = PairSpec_Gated->GetBinContent(n);
+	Double_t gated_dly = PairSpec_GatedDly->GetBinContent(n);
+	Double_t taggeff = (gated-gated_dly)/open;
+	if(!TMath::Finite(taggeff))
           taggeff = 0;
-      cmd << taggeff << " ";
+	cmd << taggeff << " ";
+      }
+      cmd << " > /dev/null";
+      system(cmd.str().c_str());
     }
-    cmd << " > /dev/null";
-    system(cmd.str().c_str());
+  }
+
+  // determine percent of events with a hardware error
+  if(gROOT->FindObject("NHardwareError")){
+    if((NHardwareError->Integral()) > 0){
+      stringstream cmd;
+      Double_t per_err = (100*(1.-((NHardwareError->GetBinContent(1))/(NHardwareError->Integral()))));
+      cmd << "caput GEN:MON:EventsWithError.A " << per_err << " > /dev/null";
+      system(cmd.str().c_str());
+      NHardwareError->Reset();
+    }
   }
 }
