cea185e4c3a0ab07c80c830717d7c8fdf4859b49
Andreas Neiser <neiser@kph.uni-mainz.de>
2014-09-22 18:03:47 +0200

    Calib: AddBeamtime.C should not AddRuns by default, made config file paths relative

--- CaLib/macros/AddBeamtime.C
+++ CaLib/macros/AddBeamtime.C
@@ -22,20 +22,21 @@ void AddBeamtime()
  
     // macro configuration: just change here for your beamtime and leave
     // the other parts of the code unchanged
-    const Char_t rawfilePath[]      = "/kernph/data/A2/LD2/May_09";
-    const Char_t target[]           = "LD2";
-    const Int_t firstRun            = 22626;
-    const Int_t lastRun             = 23728;
-    const Char_t calibName[]        = "LD2_May_09";
-    const Char_t calibDesc[]        = "Standard calibration for May 2009 beamtime";
-    const Char_t calibFileTagger[]  = "/usr/users/werthm/AcquRoot/acqu/acqu/data/May_09/Tagger/FP.dat";
-    const Char_t calibFileCB[]      = "/usr/users/werthm/AcquRoot/acqu/acqu/data/May_09/CB/NaI.dat";
-    const Char_t calibFileTAPS[]    = "/usr/users/werthm/AcquRoot/acqu/acqu/data/May_09/TAPS/BaF2_PWO.dat";
-    const Char_t calibFilePID[]     = "/usr/users/werthm/AcquRoot/acqu/acqu/data/May_09/PID/PID.dat";
-    const Char_t calibFileVeto[]    = "/usr/users/werthm/AcquRoot/acqu/acqu/data/May_09/TAPS/Veto.dat";
+    const Int_t firstRun            = 4921;
+    const Int_t lastRun             = 6008;
+    const Char_t calibName[]        = "2014-07_EPT_Prod_Neiser";
+    const Char_t calibDesc[]        = "Calibration for July 2014 EPT Beamtime";
+    const Char_t calibFileTagger[]  = "../acqu_user/data/Detector-EPT.dat";
+    const Char_t calibFileCB[]      = "../acqu_user/data/Detector-NaI.dat";
+    const Char_t calibFileTAPS[]    = "../acqu_user/data/Detector-BaF2-PbWO4.dat";
+    const Char_t calibFilePID[]     = "../acqu_user/data/Detector-PID.dat";
+    const Char_t calibFileVeto[]    = "../acqu_user/data/Detector-Veto.dat";
 
+    // Adding the runfiles should be done by maintainers only!
     // add raw files to the database
-    TCMySQLManager::GetManager()->AddRunFiles(rawfilePath, target);
+    //const Char_t rawfilePath[]      = "/kernph/data/A2/LD2/May_09";
+    //const Char_t target[]           = "LD2";    
+    //TCMySQLManager::GetManager()->AddRunFiles(rawfilePath, target);
     
     // read AcquRoot calibration of tagger
     TCMySQLManager::GetManager()->AddCalibAR(kDETECTOR_TAGG, calibFileTagger,
