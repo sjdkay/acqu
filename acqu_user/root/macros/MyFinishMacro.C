5605fb488ce6a887514395b0f50954d704a10946
Dominik Werthmueller <dominik.werthmueller@unibas.ch>
2014-11-12 18:52:57 +0100

    updated MyFinishMacro.C

--- acqu_user/root/macros/MyFinishMacro.C
+++ acqu_user/root/macros/MyFinishMacro.C
@@ -1,15 +1,12 @@
-// SVN Info: $Id: MyFinishMacro.C 781 2011-02-03 13:39:41Z werthm $
-
 /*************************************************************************
- * Author: Dominik Werthmueller, 2007-2008                               *
+ * Author: Dominik Werthmueller, 2007-2011                               *
  *************************************************************************/
 
 //////////////////////////////////////////////////////////////////////////
 //                                                                      //
 // MyFinishMacro.C                                                      //
 //                                                                      //
-// Save all histograms and the OSCAR A2 event tree to a ROOT file.      //
-// This macro should be called in batch mode using ARBatchAnalysis.     //
+// Save all histograms to a ROOT file.                                  //
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
 
@@ -75,6 +72,36 @@ Char_t* ExtractPureFileName(const Char_t* s)
 }
 
 //______________________________________________________________________________
+Char_t* ExtractPureFileNameParDirPrefix(const Char_t* s)
+{
+    // Extracts the file name and truncates the file ending of a file given 
+    // by its full Unix path in the string s (taken from OSCAR's TOSUtils).
+    // Use the parent directory as prefix.
+    
+    Char_t tmp[256];
+    Char_t tmp2[256];
+
+    // Create output string if empty
+    if (!fTmpCh) fTmpCh = new Char_t[256];
+    
+    // Search last slash
+    Int_t pos = LastIndexOf(s, '/');
+
+    // get string until filename
+    strncpy(tmp, s, pos);
+    tmp[pos]='\0';
+
+    // get parent directory
+    strcpy(tmp2, ExtractFileName(tmp));
+
+    // create new string
+    sprintf(tmp, "%s_%s", tmp2, ExtractPureFileName(s));
+    strcpy(fTmpCh, tmp);
+
+    return fTmpCh;
+}
+
+//______________________________________________________________________________
 Bool_t IsOSCARTree()
 {
     // Check if an A2 OSCAR Tree was created in the analysis.
@@ -103,21 +130,20 @@ void CorrectTaggerScalers(Bool_t useNewScalers)
     //       kFALSE  : use old scaler pairs
   
     Char_t name[256];
-
-    // change into the main ROOT memory directory
-    gROOT->cd();
+    
+    // create total scaler histogram name
+    sprintf(name, "SumScalers0to%d", gAR->GetMaxScaler());
 
     // load histograms
-    //TH1F* hSc  = (TH1F*) gDirectory->Get("SumScalers0to632");
-    TH1F* hSc  = (TH1F*) gDirectory->Get("SumScalers0to139");
-    TH1F* hScC = (TH1F*) gDirectory->Get("CorrectedSumScalers");
-    TH1F* hScT = (TH1F*) gDirectory->Get("FPD_ScalerAcc");
+    TH1* hSc  = gDirectory->Get(name);
+    TH1* hScC = gDirectory->Get("CorrectedSumScalers");
+    TH1* hScT = (TH1*) gDirectory->Get("FPD_ScalerAcc");
+    if (!hScT) hScT = (TH1*) gROOT->Get("FPD_ScalerAcc");
 
     // check if all histograms are here
     if (!hSc) 
     {
-      //printf("ERROR: Could not load the 'SumScalers0to632' histograms which is required for the tagger scaler correction!\n");
-        printf("ERROR: Could not load the 'SumScalers0to139' histograms which is required for the tagger scaler correction!\n");
+        printf("ERROR: Could not load the '%s' histograms which is required for the tagger scaler correction!\n", name);
         return;
     }
     if (!hScC) 
@@ -134,15 +160,12 @@ void CorrectTaggerScalers(Bool_t useNewScalers)
     // load scaler values
     Double_t sc_0_ar   = hSc->GetBinContent(1);
     Double_t sc_1_ar   = hSc->GetBinContent(2);
-    Double_t sc_14_ar  = hSc->GetBinContent(15);
-    /*
     Double_t sc_144_ar = hSc->GetBinContent(145);
     Double_t sc_145_ar = hSc->GetBinContent(146);
     Double_t sc_534_ar = hSc->GetBinContent(535);
     Double_t sc_535_ar = hSc->GetBinContent(536);
     Double_t sc_528_ar = hSc->GetBinContent(529);
     Double_t sc_529_ar = hSc->GetBinContent(530);
-    */
 
     Double_t sc_0   = hScC->GetBinContent(1);
     Double_t sc_1   = hScC->GetBinContent(2);
@@ -151,18 +174,10 @@ void CorrectTaggerScalers(Bool_t useNewScalers)
 
     // calculate livetimes and the correction factor
     Double_t live_tot, live_tag, corr_fac;
-    /*
     if (useNewScalers)
     {
         live_tot = sc_528_ar / sc_529_ar;
         live_tag = sc_535_ar / sc_534_ar;
-        corr_fac = live_tot / live_tag;
-    }
-    */
-    if (useNewScalers)
-    {
-        live_tot = sc_1_ar / sc_0_ar;
-        live_tag = sc_14_ar / sc_0_ar;
         corr_fac = live_tot / live_tag;
     }
     else
@@ -176,8 +191,11 @@ void CorrectTaggerScalers(Bool_t useNewScalers)
     // in offline ROOT file processing
     if (!gAR->IsOnline()) corr_fac /= 2.;
     
+    // calculate error
     Double_t fc2 = corr_fac * corr_fac;
-    Double_t corr_fac_err = TMath::Sqrt(fc2/sc_0 + fc2/sc_1 + fc2/sc_144 + fc2/sc_145);
+    Double_t corr_fac_err;
+    if (useNewScalers) corr_fac_err = TMath::Sqrt(fc2/sc_529_ar + fc2/sc_528_ar + fc2/sc_534_ar + fc2/sc_535_ar);
+    else corr_fac_err = TMath::Sqrt(fc2/sc_0 + fc2/sc_1 + fc2/sc_144 + fc2/sc_145);
 
     // correct all tagger channels
     Int_t n_corr = 0;
@@ -240,20 +258,12 @@ void CorrectTaggerScalers(Bool_t useNewScalers)
     printf("\n");
     printf("------- Tagger scaler correction -------\n");
     printf("\n");
-    /*
     if (useNewScalers)
     {
         printf("Scaler 528 (CB live) : %e \n", sc_528_ar);
         printf("Scaler 529 (CB free) : %e \n", sc_529_ar);
         printf("Scaler 534 (FP free) : %e \n", sc_534_ar);
         printf("Scaler 535 (FP live) : %e \n", sc_535_ar);
-    }
-    */
-    if (useNewScalers)
-    {
-        printf("Scaler 0  (CB free) : %e \n", sc_0_ar);
-        printf("Scaler 1  (CB live) : %e \n", sc_1_ar);
-        printf("Scaler 14 (FP live) : %e \n", sc_14_ar);
     }
     else
     {
@@ -281,54 +291,68 @@ void MyFinishMacro()
     
     Char_t outFileName[256];
     
-    cout << endl;
-    cout << "End of file: Executing MyFinishMacro ..." << endl;
+    printf("\nEnd of file: Executing MyFinishMacro ...\n");
     
-    // get the run number
-    Int_t run = ((TA2MyPhysics*)gAR->GetAnalysis()->GetPhysics())->GetRunNumber();
-
-    // Correct tagger scalers 
-    CorrectTaggerScalers(kTRUE);
-    //CorrectTaggerScalers(kFALSE);
-    //if (run > 20000) CorrectTaggerScalers(kTRUE);
-    
-    // Check if an A2 OSCAR tree was created
-    if (IsOSCARTree())
-    {
-        // get the ROOT file from the A2 OSCAR tree
-        TTree* t = gAR->GetAnalysis()->GetPhysics()->GetTree();
-        TFile* f = t->GetCurrentFile();
-        
-        // Save histograms to file and close it
-        f->cd();
-        f->Write();
-        gROOT->GetList()->Write();
-        f->Close();
-        
-        cout << "A2 OSCAR Tree was found." << endl;
-        cout << "All histograms and tree saved to " << f->GetName() << endl << endl;
-    }
-    else
+    // check for physics analysis
+    if (!gAR->GetAnalysis()->GetPhysics())
     {
         // Build the output file name depending on the type of analysis
         if (gAR->IsOnline()) sprintf(outFileName, "%sARHistograms_%s.root", gAR->GetTreeDir(), 
                                      ExtractPureFileName(gAR->GetFileName()));
-        else sprintf(outFileName, "%sARHistograms_%s.root", gAR->GetTreeDir(), 
-                     ExtractPureFileName(gAR->GetTreeFile()->GetName()));
-        
-        // Save histograms to file and close it
-        TFile f(outFileName, "recreate");
-        f.SetCompressionLevel(4);
-        gROOT->GetList()->Write();
-        f.Close();
+        else 
+            sprintf(outFileName, "%sARHistograms_%s.root", gAR->GetTreeDir(), 
+                    ExtractPureFileNameParDirPrefix(gAR->GetTreeFile()->GetName()));
 
-        cout << "All histograms saved to " << outFileName << endl;
+        // create output file and save objects
+        TFile* f = new TFile(outFileName, "recreate");
+        f->SetCompressionLevel(4);
+        gROOT->GetList()->Write();
+        printf("Saved all histograms to %s\n", f->GetName());
+        delete f;
     }
-    
-    // clear histograms
-    //gAN->ZeroAll();
-    //cout << "Resetting histograms" << endl << endl;
+    else
+    {
+        // get the run number
+        TA2MyPhysics* ana = (TA2MyPhysics*)gAR->GetAnalysis()->GetPhysics();
+        Int_t run = 0;
+        if (ana) run = ana->GetRunNumber();
+        
+        // Correct tagger scalers for different dead-times in the tagger DAQ and
+        // the CB DAQ, respectively.
+        // On September 24 2008 a new pair of scalers was installed for this purpose
+        // (after run 19000 (September 25 2008) this should work).
+        CorrectTaggerScalers(kFALSE);
+        if (run > 19000) CorrectTaggerScalers(kTRUE);
  
+        // Check if output file is already open
+        TFile* f;
+        if (f = ana->GetOutputFile())
+        {
+            // Save histograms to file and close it
+            f->Write();
+            f->cd();
+            gROOT->GetList()->Write();
+            printf("Saved all histograms to %s\n", f->GetName());
+            delete f;
+        }
+        else
+        {
+            // Build the output file name depending on the type of analysis
+            if (gAR->IsOnline()) sprintf(outFileName, "%sARHistograms_%s.root", gAR->GetTreeDir(), 
+                                         ExtractPureFileName(gAR->GetFileName()));
+            else 
+                sprintf(outFileName, "%sARHistograms_%s.root", gAR->GetTreeDir(), 
+                        ExtractPureFileNameParDirPrefix(gAR->GetTreeFile()->GetName()));
+
+            // Save histograms to file and close it
+            f = new TFile(outFileName, "recreate");
+            f->SetCompressionLevel(4);
+            gROOT->GetList()->Write();
+            printf("Saved all histograms to %s\n", f->GetName());
+            delete f;
+        }
+    }
+
     gSystem->Exit(0);
 }
 
